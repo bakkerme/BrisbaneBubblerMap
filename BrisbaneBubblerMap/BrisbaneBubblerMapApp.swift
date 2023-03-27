a13437610e7c1275c2e4d0c06785c8718c3d3ae4
Brandon Bakker <brandon@bdmd.com.au>
2023-03-27 18:06:53 +1000

    Initial Commit

---
+++ BrisbaneBubblerMap/BrisbaneBubblerMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrisbaneBubblerMapApp.swift
+//  BrisbaneBubblerMap
+//
+//  Created by Brandon Bakker on 27/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct BrisbaneBubblerMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrisbaneBubblerMap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BrisbaneBubblerMap
+//
+//  Created by Brandon Bakker on 27/3/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
